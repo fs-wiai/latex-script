6126e156d3b362d4c1d0d4c219baee08dfce99d0
Florian Knoch <florian.knoch@stud.uni-bamberg.de>
2021-04-01 14:06:28 +0200

    Add source code listings exercise

---
+++ exercises/source-code-listings/Source.java
@@ -0,0 +1,9 @@
+public class Source {
+  public static void main(String[] args) {
+    String greeting = "Willkommen in dieser äußerst sinnvollen Java-Klasse, die wirklich bemerkenswert wenig Bedeutung trägt.";
+
+    for (int i = 0; i < greeting.length(); i++) {
+      System.out.println(greeting.charAt(i));
+    }
+  }
+}
